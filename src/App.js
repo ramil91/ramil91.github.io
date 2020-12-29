aee21f6bd33c4a4c37d3672ef039dc0558e8bd98
Ramil Ahmad Malik <55125754+ramil91@users.noreply.github.com>
2020-12-29 18:30:56 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-         Testing a paragraph
+         Testing a paragraph for second time
         </p>
         <a
           className="App-link"
